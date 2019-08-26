1a653df19a7c38e038e859cf1166d7a018333828
RobertJrIII <roberto.gongalez02@gmail.com>
2019-08-25 18:14:19 -0700

    added navbar

---
+++ app/src/main/java/com/dev/rj3/fantasma/AccountFragment.java
@@ -0,0 +1,76 @@
+package com.dev.rj3.fantasma;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.dev.rj3.fantasma.reddit_api.RedditAPI;
+import com.dev.rj3.fantasma.reddit_api.model.Feed;
+import com.dev.rj3.fantasma.reddit_api.model.children.Children;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class AccountFragment extends Fragment {
+
+    private static final String BASE_URL = "https://www.reddit.com/";
+    private TextView textView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.account, container, false);
+        textView = view.findViewById(R.id.textView);
+        return view;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        RedditAPI redditAPI = retrofit.create(RedditAPI.class);
+        Call<Feed> call = redditAPI.getData();
+
+        call.enqueue(new Callback<Feed>() {
+            @Override
+            public void onResponse(Call<Feed> call, Response<Feed> response) {
+
+                if (!response.isSuccessful()) {
+                    textView.setText("Code: " + response.code());
+                    return;
+                }
+
+                ArrayList<Children> childrenList = response.body().getData().getChildren();
+                for (Children children : childrenList) {
+                    String content = "";
+                    content += " " + children.getEntry().getTitle() + "\n";
+                    content += " " + children.getEntry().getSubreddit_name_prefixed() + "\n";
+                    content += " " + children.getEntry().getAuthor() + "\n";
+                    content += " " + children.getEntry().getScore() + "\n\n";
+                    textView.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Feed> call, Throwable t) {
+                textView.setText(t.getMessage());
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/dev/rj3/fantasma/MainActivity.java
+++ app/src/main/java/com/dev/rj3/fantasma/MainActivity.java
@@ -1,13 +1,16 @@
 package com.dev.rj3.fantasma;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.dev.rj3.fantasma.reddit_api.RedditAPI;
 import com.dev.rj3.fantasma.reddit_api.model.Feed;
 import com.dev.rj3.fantasma.reddit_api.model.children.Children;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
@@ -18,45 +21,25 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String BASE_URL = "https://www.reddit.com/";
-    private TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.textView);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AccountFragment()).commit();
 
-        Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        RedditAPI redditAPI = retrofit.create(RedditAPI.class);
-        Call<Feed> call = redditAPI.getData();
+        BottomNavigationView bottomNav = findViewById(R.id.navBar);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
 
-        call.enqueue(new Callback<Feed>() {
-            @Override
-            public void onResponse(Call<Feed> call, Response<Feed> response) {
 
-                if (!response.isSuccessful()) {
-                    textView.setText("Code: " + response.code());
-                    return;
-                }
-
-                ArrayList<Children> childrenList = response.body().getData().getChildren();
-                for (Children children : childrenList) {
-                    String content = "";
-                    content += " " + children.getEntry().getTitle() + "\n";
-                    content += " " + children.getEntry().getSubreddit_name_prefixed() + "\n";
-                    content += " " + children.getEntry().getAuthor() + "\n";
-                    content += " " + children.getEntry().getScore() + "\n\n";
-                    textView.append(content);
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Feed> call, Throwable t) {
-                textView.setText(t.getMessage());
-            }
-        });
     }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            return false;
+        }
+    };
 }
