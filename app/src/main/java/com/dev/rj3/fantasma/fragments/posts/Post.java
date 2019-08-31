de82cfdd7dbea2a4219444e163f6c4eb544f0d5d
RobertJrIII <roberto.gongalez02@gmail.com>
2019-08-31 10:55:18 -0700

    have functional layout

--- app/src/main/java/com/dev/rj3/fantasma/fragments/posts/Post.java
+++ app/src/main/java/com/dev/rj3/fantasma/fragments/posts/Post.java
@@ -3,14 +3,18 @@ package com.dev.rj3.fantasma.fragments.posts;
 public class Post {
 
     private String title;
-    private String subreddit;
+    private String subreddit, author;
 
 
     public Post(String title, String subreddit) {
         this.title = title;
         this.subreddit = subreddit;
+        //this.author = author;
     }
 
+    public String getAuthor() {
+        return author;
+    }
 
     public String getTitle() {
         return title;
